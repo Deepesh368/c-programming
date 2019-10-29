fba5d8479a41d90d19c90e0d1c176f34e102c621
Deepesh368 <56600807+Deepesh368@users.noreply.github.com>
2019-10-30 00:11:15 +0530

    Update 3.c
    I made a few changes to the program and added beautified the code a little.

--- 3.c
+++ 3.c
@@ -1,34 +1,36 @@
 
 #include <stdio.h> // Including standard input output header file
 #include <stdlib.h> 
-
 #include <string.h>
 
-int main()
+int main(void)
 {
 	char *string1, *string2, *string; //declaring the pointers for dynamic memory allocation//
 
-	string1 = (char*)malloc(255*sizeof(char)); // Initializing string 1 //
-	string2 = (char*)malloc(255*sizeof(char)); // Initializing string 2 //
+	string1 = (char*)malloc(255 * sizeof(char)); // allocating memory for string 1 //
+	string2 = (char*)malloc(255 * sizeof(char)); // Initializing string 2 //
 	
 
-	scanf("%s",string1);	//reading the strings into the alloted memories// 
-	scanf("%s",string2);
+	scanf("%s %s",string1, string2);	//reading the strings into the alloted memories// 
+        int len1, len2;
+	len1 = strlen(string1); // length of string1 //
+	len2 = strlen(string2); // length of string2 //
+	string = (char*)malloc((len1 + len2 + 1) * sizeof(char));  //string is memory alloted to store the concatanated strings//
 
-	int len1 = strlen(string1); // length of string1 //
-	int len2 = strlen(string2); // length of string2 //
-	string = (char*)calloc(len1 + len2 + 1,sizeof(char));  //string is memory alloted to store the concatanated strings//
-
-	for(int i = 0;i <len1;i++)   // inserting string1 in string //
+	for(int i = 0;i < len1;i++)   // inserting string1 in string //
 	{
 		string[i] = string1[i];
 	}
 
-	for(int i = 0;i < len2;i++) // inserting string2 in string //
+	for(int j = 0;j < len2;j++) // inserting string2 in string //
 	{
-		string[len1] = string2[i];
+		string[len1] = string2[j];
 		len1++;
 	}
 	
-	printf("%s",string); // Printing the string //
+	printf("%s", string); // Printing the string //
+	free(string1);  /*We should free the memory before exiting the program as it helps conserve space.*/
+	free(string2);
+	free(string);
+	return 0;
 }
